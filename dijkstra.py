d52be22531ddd057e626db528c723715c1b813db
QuartzExe <athitkrishna41@gmail.com>
2025-10-26 03:26:08 +0530

    Dijkstra Algorithm for a given adjacency list

---
+++ dijkstra.py
@@ -0,0 +1,26 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances={}
+
+    for node in graph:
+       distances[node] = float('inf')
+
+    distances[start]=0
+
+    heap = [(0, start)]
+
+    while heap:
+        current_distance, current_node = heapq.heappop(heap)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(heap, (distance, neighbor))
+
+    return distances
